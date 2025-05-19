455a5824bae113112d5c4621b68b5b21073be743
Lzbdc <150091473+Lzbdc@users.noreply.github.com>
2025-05-19 10:50:14 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 import {sqlite3} from 'sqlite3';
 import {open} from 'sqlite';
+
+function criarEPolularTabelaUsuarios (){
+  open({
+    filename: './banco.db',
+    driver: 'sqlite3.driver'
+  })
+}
