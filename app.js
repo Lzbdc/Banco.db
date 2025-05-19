fa9eef86a095c582016d2e5751eabaa53f8f9c2c
Lzbdc <150091473+Lzbdc@users.noreply.github.com>
2025-05-19 10:52:27 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import {sqlite3} from 'sqlite3';
 import {open} from 'sqlite';
 
-async function criarEPolularTabelaUsuarios (){
+async function criarEPolularTabelaUsuarios (nome, sobrenome){
   const db = await open({
     filename: './banco.db',
     driver: 'sqlite3.driver'
