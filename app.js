bda09631ad152f33e024412b1e06494ddc6224c8
Lzbdc <150091473+Lzbdc@users.noreply.github.com>
2025-05-19 11:28:27 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ async function criarEPolularTabelaUsuarios (nome, sobrenome){
     driver: 'sqlite3.Database',
   })
   db.run(`CREATE TABLE IF NOT EXISTS usuarios (id INTEGER PRIMARY KEY, nome TEXT, sobrenome TEXT)`)
-  db.run(`INSERT INTO usuario (nome, sobrenome) VALUES ?,?`
+  db.run(`INSERT INTO usuario (nome, sobrenome) VALUES (?,?)`
          [
            nome, 
            sobrenome
